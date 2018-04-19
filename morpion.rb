d3dbe2a1ff60c6cbec096ccba360936b88b69a4e
Thomas Szadek <37246650+thomisz@users.noreply.github.com>
2018-04-19 16:42:22 +0200

    Add files via upload

---
+++ morpion.rb
@@ -0,0 +1,52 @@
+require "backports"# choisir aléatoirement le joueur qui va commencer la partie ok gem installé
+class Game (jeu)
+    def initialize(xox)
+      class players
+        def initialize(player1, player2)
+        @name1=player1
+        @name2=player2
+        # chaque joueur entre son prénom
+        puts "prénom du joueur 1"
+        gets.chomp
+        {name1}
+        puts "prénom du joueur 2"
+        gets.chomp
+        {name2}
+        #attribuer un type de jeton à chaque joueur
+        @name1=x
+        @nama2=o
+        # l'algorithme décide de façon aléatoire de celui qui commence
+        r = Random.new
+        r.rand(1...2)
+        r.bytes()
+        # annonce le nom joueur qui a été tiré au sort pour commencer
+        puts "Bravo {NAME}, tu commences"
+        class board
+          def to_s
+            #définir l'espace de jeu: nombre de colonnes et de lignes
+            row=[1,2,3]
+            colonnes=[1,2,3]
+       class game
+         def
+           #lancement de la partie
+           number 1= [1,1]
+           number 2= [1,2]
+           ...
+           number 9 = [3,3]
+           #chiffre hors tableau
+           if number >9
+             Puts "Invalide"
+            #combinaisons gagnantes (OU)
+              #3 colonnes: colonne 1 remplie par le même joueur ou colonne 2 remplie par le même joueur ou colonne 3 remplie par le même joueur
+              #3 lignes: idem dans le raisonnement
+              # 2 diagonales: la première [1;3] [2;2] [3;1] ou la deuxième [1;1] [2;2] [3;3]
+        # deux possibilités de résultat: partie gagnée ou match nul
+        if   win
+          puts "Bravo vous avez gagné!"
+        elsif  game over
+          puts "Aucun gagnant! veuillez recommencer une partie!"
+        end
+      end
+    end
+  end
+end
